2021f97a0a2db0452541866a501cbadc7da49a71
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-18 15:44:31 +0100

    feat: runtime work started

---
+++ packages/runtime/src/h.js
@@ -0,0 +1,15 @@
+
+export const DOM_TYPES = {
+  TEXT: 'text',
+  ELEMENT: 'element',
+  FRAGMENT: 'fragment'
+}
+
+export function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT
+  }
+}
---
+++ packages/runtime/src/mount-dom.js
@@ -0,0 +1,40 @@
+import { DOM_TYPES } from "./h";
+
+
+export function mountDOM(vdom, parentEl) {
+  switch (vdom.type) {
+    case DOM_TYPES.TEXT: {
+      createTextNode(vdom, parentEl)
+      break;
+    }
+    case DOM_TYPES.ELEMENT: {
+      createElementNode(vdom, parentEl)
+      break;
+    }
+    case DOM_TYPES.FRAGMENT: {
+      createFragmentNodes(vdom, parentEl)
+      break
+    }
+    default: {
+      throw new Error(`Can't mound a DOM of type: ${vdom.type}`)
+    }
+  }
+}
+
+function createTextNode(vdom, parentEl) {
+  const { value } = vdom
+
+  const textNode = document.createTextNode(value)
+  vdom.el = textNode
+
+  parentEl.append(textNode)
+}
+
+function createFragmentNodes(vdom, parentEl) {
+  const { children } = vdom
+
+  vdom.el = parentEl
+
+  children.forEach((child) => mountDOM(child, parentEl))
+
+}
